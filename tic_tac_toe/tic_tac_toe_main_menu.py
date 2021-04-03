1a37a84d021f6ee47af7fa78354743eace6afd5f
Ana Ajuda <ana.margarida.ajuda@hotmail.com>
2021-04-03 19:03:39 +0100

    Bot settings is the only thing missing now
    Also added some notes and modified code structure in order to become more organized

--- tic_tac_toe/tic_tac_toe_main_menu.py
+++ tic_tac_toe/tic_tac_toe_main_menu.py
@@ -1,3 +1,8 @@
+#NOTE: NA ALTURA DE TESTAR NA MESA: 
+# - REMOVER LINHAS 1 E 19 ('OS' DEIXA DE SER NECESSÁRIO)
+# - COLOCAR FULLSCREEN = True (linha 26)
+# - DEFINIR LIMITE PARA NÚMERO DE BOARDSIZE
+
 import os
 import pygame
 from Color import *
@@ -5,19 +10,10 @@ from Galo import *
 
 FPS = 60 #Sets a limit of FPS to be able to run better
 
-#Colours
-# BLACK = (255, 255, 255)
-# LIGHT_BLACK = (20, 20, 20)
-# GREY = (28, 27, 27)
-# WHITE = (207, 207, 207)
-# LIGHT_GREEN = (109, 215, 143)
-# DARK_GREEN = (79, 161, 106)
-
 #Get fonts
 FONT_ORIGAMI = 'fonts/Origami.ttf'
 FONT_RAJDHANI = 'fonts/Rajdhani-Medium.ttf'
 FONT_RAJDHANI_BOLD = 'fonts/Rajdhani-Bold.ttf'
-
 
 #DEFINING SCREEN, BUTTONS, TEXT AND LAYOUTS
 
@@ -31,7 +27,7 @@ FULLSCREEN = False
 if FULLSCREEN == True:
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
 else:
-    SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN_WIDTH, SCREEN_HEIGHT = int(0.75 * SCREEN_WIDTH), int(0.75 * SCREEN_HEIGHT)
+    SCREEN_WIDTH, SCREEN_HEIGHT = int(0.75 * SCREEN_WIDTH), int(0.75 * SCREEN_HEIGHT)
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 #Create buttons (according to screen size)
@@ -45,11 +41,17 @@ MAIN_BUTTONS_LAYOUT = [((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 5 //
                        ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 7 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
                        ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 9 // 12, BUTTON_WIDTH, BUTTON_HEIGHT)]
 
-SETTINGS_BUTTONS_LAYOUT = [((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 5 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
-                           ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 7 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
+START_GAME_BUTTONS_LAYOUT = [((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 5 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
+                             ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 7 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
+                             ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 9 // 12, BUTTON_WIDTH, BUTTON_HEIGHT)]
+
+SETTINGS_BUTTONS_LAYOUT = [((SCREEN_WIDTH - BUTTON_WIDTH) // 4, SCREEN_HEIGHT * 5 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
+                           ((SCREEN_WIDTH - BUTTON_WIDTH) // 4, SCREEN_HEIGHT * 7 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
+                           ((SCREEN_WIDTH - BUTTON_WIDTH) // 1.35, SCREEN_HEIGHT * 5 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
+                           ((SCREEN_WIDTH - BUTTON_WIDTH) // 1.35, SCREEN_HEIGHT * 7 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
                            ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 9 // 12, BUTTON_WIDTH, BUTTON_HEIGHT)]
 
-NUM_PLAYERS_BUTTONS_LAYOUT = [((SCREEN_WIDTH - BUTTON_WIDTH) // 2.28, SCREEN_HEIGHT * 4 // 12, SMALL_BUTTON_WIDTH, SMALL_BUTTON_WIDTH),
+CHOOSE_SET_BUTTONS_LAYOUT = [((SCREEN_WIDTH - BUTTON_WIDTH) // 2.28, SCREEN_HEIGHT * 4 // 12, SMALL_BUTTON_WIDTH, SMALL_BUTTON_WIDTH),
                              ((SCREEN_WIDTH - BUTTON_WIDTH) // 1.16, SCREEN_HEIGHT * 4 // 12, SMALL_BUTTON_WIDTH, SMALL_BUTTON_WIDTH),
                              ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 7 // 12, BUTTON_WIDTH, BUTTON_HEIGHT),
                              ((SCREEN_WIDTH - BUTTON_WIDTH) // 2, SCREEN_HEIGHT * 9 // 12, BUTTON_WIDTH, BUTTON_HEIGHT)]
@@ -68,7 +70,6 @@ MEDIUM_BOLD_TEXT = pygame.font.Font(FONT_RAJDHANI_BOLD, int(45 / 1440 * SCREEN_H
 SMALL_TEXT = pygame.font.Font(FONT_RAJDHANI, int(35 / 1440 * SCREEN_HEIGHT))
 SMALL_BOLD_TEXT = pygame.font.Font(FONT_RAJDHANI_BOLD, int(35 / 1440 * SCREEN_HEIGHT))
 LABEL_TEXT = pygame.font.Font(FONT_RAJDHANI, int(85 / 1440 * SCREEN_HEIGHT))
-
 
 #BUTTONS AND TEXTS FUNCTIONS
 
@@ -101,6 +102,137 @@ def button(text, x, y, w, h, click, inactive_button = LIGHT_GREEN, active_button
     pygame.display.update()
     return button_clicked
 
+#CHOOSING SETTINGS MENU
+def choose_settings_menu(settings_type):
+    
+    def setup_choose_settings_menu(settings_type):
+        SCREEN.fill(GREY)
+        
+        if settings_type == 'number of players':
+            text_surf, text_rectangle = text_objects('Select the ' +  str(settings_type) + ' (max of 9) ...', LARGE_TEXT, WHITE)
+        else:
+            text_surf, text_rectangle = text_objects('Select the ' +  str(settings_type) + ' ...', LARGE_TEXT, WHITE)
+
+        text_rectangle.center = ((SCREEN_WIDTH // 3.27), (SCREEN_HEIGHT // 5))
+        SCREEN.blit(text_surf, text_rectangle)
+        
+        pygame.display.update()
+
+    def choose_settings(settings_display):
+        pygame.draw.rect(SCREEN, LIGHT_GREEN, (int(SCREEN_WIDTH // 2.72), int(SCREEN_HEIGHT * 4 // 12), int(SCREEN_WIDTH * 0.8 // 3), int(70)), int(1))
+        pygame.draw.rect(SCREEN, GREY, (int(SCREEN_WIDTH // 2.66), int(SCREEN_HEIGHT * 4.1 // 12), int(SCREEN_WIDTH * 0.75 // 3), int(85 / 1440 * SCREEN_HEIGHT)))
+
+        text_surf, text_rectangle = text_objects(str(settings_display), LABEL_TEXT, WHITE)
+        text_rectangle.center = (int(SCREEN_WIDTH // 2), int(SCREEN_HEIGHT // 2.67))
+        SCREEN.blit(text_surf, text_rectangle)
+
+    def changes_done(settings_display, number):
+        pygame.draw.rect(SCREEN, GREY, (int(SCREEN_WIDTH / 1.355), int(SCREEN_HEIGHT * 0.935), int(1 / 4 * SCREEN_WIDTH), int(50 / 1440 * SCREEN_HEIGHT)))
+        
+        text_surf, text_rectangle = text_objects('Succesfully changed ' + str(settings_display) + ' to ' + str(number), SMALL_TEXT, WHITE)
+        text_rectangle.center = (int(SCREEN_WIDTH / 1.16), int(SCREEN_HEIGHT * 0.95))
+        SCREEN.blit(text_surf, text_rectangle)
+        pygame.display.update()
+        pygame.time.delay(1000)
+
+    setup_choose_settings_menu(settings_type)
+    if settings_type == 'number of players':
+        num_players = 2
+        choose_settings(num_players)
+    elif settings_type == 'number of rounds':
+        num_rounds = 3
+        choose_settings(num_rounds)
+    elif settings_type == 'board size':
+        num_board = 3
+        choose_settings(num_board)
+
+    settingsmenu = True
+    while settingsmenu:
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+
+        if button(' - ', *CHOOSE_SET_BUTTONS_LAYOUT[0], click):
+            pygame.time.delay(100)
+            
+            if settings_type == 'number of players':
+                if num_players <= 2:
+                    pass
+                elif num_players > 1:
+                    num_players -= 1
+                choose_settings(num_players)
+
+            elif settings_type == 'number of rounds':
+                if num_rounds <= 1:
+                    pass
+                elif num_rounds > 1:
+                    num_rounds -= 1
+                choose_settings(num_rounds)
+
+            elif settings_type == 'board size':
+                if num_board <= 3:
+                    pass
+                elif num_board > 3:
+                    num_board -= 1
+                choose_settings(num_board)
+
+        elif button(' + ', *CHOOSE_SET_BUTTONS_LAYOUT[1], click):
+            pygame.time.delay(100)
+            
+            if settings_type == 'number of players':
+                if num_players >= 9:
+                    pass
+                elif num_players < 9:
+                    num_players += 1
+                choose_settings(num_players)
+            
+            elif settings_type == 'number of rounds':
+                num_rounds += 1
+                choose_settings(num_rounds)
+
+            elif settings_type == 'board size':
+                num_board += 1
+                choose_settings(num_board)
+
+        elif button('C O N F I R M ', *CHOOSE_SET_BUTTONS_LAYOUT[2], click):
+            pygame.time.delay(100)
+
+            if settings_type == 'number of players':
+                global NPLAYERS
+                NPLAYERS = int(num_players)
+                changes_done(settings_type, NPLAYERS)
+                return NPLAYERS
+
+            elif settings_type == 'number of rounds':
+                global NROUNDS
+                NROUNDS = int(num_rounds)
+                changes_done(settings_type, NROUNDS)
+                return NROUNDS
+
+            elif settings_type == 'board size':
+                global BOARDSIZE
+                BOARDSIZE = int(num_board)
+                changes_done(settings_type, BOARDSIZE)
+                return BOARDSIZE
+
+        elif button('R E T U R N', *CHOOSE_SET_BUTTONS_LAYOUT[3], click):
+            pygame.time.delay(100)
+            if settings_type == 'number of players':
+                return -1, -1
+            
+            elif settings_type == 'number of rounds':
+                return -1
+
+            elif settings_type == 'board size':
+                return -1
+
+        pygame.display.update(CHOOSE_SET_BUTTONS_LAYOUT)
+
+    #return NPLAYERS, NROUNDS, BOARDSIZE
+"""
 #NUMBER OF PLAYERS MENU
 def setup_nplayers_menu():
     SCREEN.fill(GREY)
@@ -257,6 +389,7 @@ def nrounds_menu():
         pygame.display.update(NUM_ROUNDS_BUTTONS_LAYOUT)
 
     return NROUNDS
+"""
 
 #SETTINGS MENU
 def setup_settings_menu():
@@ -268,10 +401,9 @@ def setup_settings_menu():
 
     pygame.display.update()
 
-def settings_menu(players, boardsize, rondas):
+def settings_menu():
     setup_settings_menu()
     
-
     settingsmenu = True
     while settingsmenu:
         click = False
@@ -284,22 +416,71 @@ def settings_menu(players, boardsize, rondas):
         
         if button('N U M B E R  O F  P L A Y E R S', *SETTINGS_BUTTONS_LAYOUT[0], click):
             pygame.time.delay(100)
-            players, boardsize = nplayers_menu()
+            NPLAYERS = choose_settings_menu('number of players')
             setup_settings_menu()
 
         elif button('N U M B E R  O F  R O U N D S', *SETTINGS_BUTTONS_LAYOUT[1], click):
             pygame.time.delay(100)
-            rondas = nrounds_menu()
+            NROUNDS = choose_settings_menu('number of rounds')
             setup_settings_menu()
 
-        elif button('R E T U R N', *SETTINGS_BUTTONS_LAYOUT[2], click):
+        elif button('B O A R D  S I Z E', *SETTINGS_BUTTONS_LAYOUT[2], click):
             pygame.time.delay(100)
-            return players, boardsize, rondas
+            BOARDSIZE = choose_settings_menu('board size')
+            setup_settings_menu()
+        
+        elif button('B O T  S E T T I N G S', *SETTINGS_BUTTONS_LAYOUT[3], click):
+            pygame.time.delay(100)
+            #BOT_DIFF = choose_settings_menu('bot difficulty')
+            #setup_settings_menu()
+
+        elif button('R E T U R N', *SETTINGS_BUTTONS_LAYOUT[4], click):
+            pygame.time.delay(100)
+            main_menu()
 
         pygame.display.update(SETTINGS_BUTTONS_LAYOUT)
         
-    return players, boardsize, rondas
+    return NPLAYERS, BOARDSIZE, NROUNDS, BOT_DIFF
 
+#START GAME MENU
+def setup_start_game():
+    SCREEN.fill(GREY)
+
+    text_surf, text_rectangle = text_objects('START GAME', MENU_TEXT, WHITE)
+    text_rectangle.center = (int(SCREEN_WIDTH // 2), int(SCREEN_HEIGHT // 4))
+    SCREEN.blit(text_surf, text_rectangle)
+
+    pygame.display.update()
+
+def start_game():
+    setup_start_game()
+    
+    startgame = True
+    while startgame:
+        click = False
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                #exit() #o exit dá me erros
+                pygame.quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+
+        if button('P L A Y  W I T H  H U M A N S', *START_GAME_BUTTONS_LAYOUT[0], click):
+            pygame.time.delay(100)
+            galo(NPLAYERS, BOARDSIZE, NROUNDS)
+            main_menu()
+
+        elif button('P L A Y  W I T H  C P U', *START_GAME_BUTTONS_LAYOUT[1], click):
+            pygame.time.delay(100)
+            galo_BOT(NPLAYERS, BOARDSIZE, NROUNDS, 0)
+            main_menu()
+
+        elif button('R E T U R N', *START_GAME_BUTTONS_LAYOUT[2], click):
+            pygame.time.delay(100)
+            main_menu()
+
+        pygame.display.update(START_GAME_BUTTONS_LAYOUT)
 
 #MAIN MENU
 def setup_main_menu():
@@ -315,18 +496,12 @@ def setup_main_menu():
     pygame.display.update()
 
 def main_menu():
-    players = 2       
-    boardsize = 3
-    rondas = 3
-    
     setup_main_menu()
-    start_game = False
-
+    
     mainmenu = True
     while mainmenu:
         click = False
         clock.tick(FPS)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 #exit() #o exit dá me erros
@@ -336,34 +511,33 @@ def main_menu():
         
         if button('S T A R T  G A M E', *MAIN_BUTTONS_LAYOUT[0], click):
             pygame.time.delay(100)
-            start_game = True
+            start_game()
 
         elif button('S E T T I N G S', *MAIN_BUTTONS_LAYOUT[1], click):
             pygame.time.delay(100)
-            playersTemp, boardsizeTemp, rondasTemp = settings_menu(players, boardsize, rondas)
-            if playersTemp != -1:
+            settings_menu()
+            """if playersTemp != -1:
                 players = playersTemp
             if boardsizeTemp != -1:
                 boardsize = boardsizeTemp
             if rondasTemp != -1:
                 rondas = rondasTemp
-            setup_main_menu()
-
+            main_menu()"""
 
         elif button('Q U I T   G A M E', *MAIN_BUTTONS_LAYOUT[2], click):
             pygame.time.delay(100)
-            #exit() # O exit dá me erro
-            pygame.quit()
-
-        if start_game:
-            galo_BOT(players,boardsize,rondas, 0)
-            main_menu()
+            exit() # O exit dá me erro
+            #pygame.quit()
 
         pygame.display.update(MAIN_BUTTONS_LAYOUT)
 
 
 pygame.display.set_caption('TIC-TAC-TOE')
 clock = pygame.time.Clock()
+NPLAYERS = 2
+BOARDSIZE = 3
+NROUNDS = 3
+BOT_DIFF = 0
 main_menu()
 
 
