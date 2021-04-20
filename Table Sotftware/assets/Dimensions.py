0106ee42e7bffed7c97ada9adcfa9966cf106ba1
Ana Ajuda <ana.margarida.ajuda@hotmail.com>
2021-04-20 22:43:02 +0100

    Now the tictactoe will only launch from main env
    PS: Added fading animation when launching games (see fade.py)
    Also please put the corresponding screen dimensions in Dimensions.py acording to your screen size.

--- Table Sotftware/assets/Dimensions.py
+++ Table Sotftware/assets/Dimensions.py
@@ -1,9 +1,17 @@
+"""
 import pygame
 
-# SCREEN_WIDTH = int(pygame.display.Info().current_w)
-# SCREEN_HEIGHT = int(pygame.display.Info().current_h)
-
-SCREEN_WIDTH = 1080
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = int(pygame.display.Info().current_w)
+SCREEN_HEIGHT = int(pygame.display.Info().current_h)
+print(SCREEN_WIDTH,SCREEN_HEIGHT)
 
 # SCREEN_WIDTH, SCREEN_HEIGHT = int(0.75 * SCREEN_WIDTH), int(0.75 * SCREEN_HEIGHT) #Tirar quando passar para a mesa
+"""
+
+#AFONSO
+#SCREEN_WIDTH = 1080
+#SCREEN_HEIGHT = 720
+
+#ANA
+SCREEN_WIDTH = 1536
+SCREEN_HEIGHT = 864
